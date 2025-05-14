a3de93ded607490af404f4ed060654beacb97248
Chun-Lin-Huang <elaine930729@gmail.com>
2025-05-14 16:06:03 +0800

    0514 update

--- models/Department.js
+++ models/Department.js
@@ -5,17 +5,30 @@ const Department = sequelize.define('Department', {
     type: DataTypes.STRING(5),
     primaryKey: true
   },
-  Name: { // Department_Name 要改成 Name
+  Name: {
     type: DataTypes.STRING(50),
     allowNull: false
   },
-  // Building: DataTypes.STRING(30),
-  // Budget: DataTypes.DECIMAL(12, 2),
-  Location: DataTypes.STRING(50),
-  Phone: DataTypes.STRING(15),
-  Established_Year: DataTypes.INTEGER,
-  Chair_ID: DataTypes.STRING(6),
-  College: DataTypes.STRING(30)
+  Location: {
+    type: DataTypes.STRING(50),
+    allowNull: true
+  },
+  Phone: {
+    type: DataTypes.STRING(15),
+    allowNull: true
+  },
+  Established_Year: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  Chair_ID: {
+    type: DataTypes.STRING(6),
+    allowNull: true
+  },
+  College: {
+    type: DataTypes.STRING(30),
+    allowNull: true
+  }
 }, {
   tableName: 'DEPARTMENT',
   timestamps: false
